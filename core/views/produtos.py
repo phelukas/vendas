3901ce7dba75084ca6e70e31df4cc2df60adcaff
phelukas <ppedrolukas19@gmail.com>
2021-06-18 15:39:59 -0300

    AJAX

--- core/views/produtos.py
+++ core/views/produtos.py
@@ -1,11 +1,15 @@
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.core.paginator import Paginator ,EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import xlwt
+from django.core import serializers
+from django.http import HttpResponse
+
 
 from core.models import Produtos, Fornecedor, Categoria
 
@@ -16,38 +20,29 @@ def get_queryset(self):
     queryset = Produtos.objects.all()
     return queryset
 
+
 def is_valid_queryparam(param):
     return param != '' and param != 'Todos' and param is not None
 
 # def ExportarExcel(queryset):
 #     response = HttpResponse(content_type='application/ms-excel')
 #     response['Content-Disposition'] = 'attachment; filename="Produtos em Falta.xls"'
-
 #     wb = xlwt.Workbook(encoding='utf-8')
 #     ws = wb.add_sheet('i')
-
 #     row_num = 0
-
 #     font_style = xlwt.XFStyle()
 #     font_style.font.bold = True
-
 #     columns = ['Id','Produto', 'Quantidade']
-
 #     for col_num in range(len(columns)):
 #         ws.write(row_num, col_num, columns[col_num], font_style)
-
-#     font_style = xlwt.XFStyle() 
-
+#     font_style = xlwt.XFStyle()
 #     queryset = queryset
-
 #     row_num = 1
-
 #     for produto in queryset:
 #         ws.write(row_num, 0, produto.id, font_style)
 #         ws.write(row_num, 1, produto.nome, font_style)
 #         ws.write(row_num, 2, produto.quantidade, font_style)
 #         row_num += 1
-
 #     # Create a StringIO object.
 #     output = StringIO.StringIO()
 #     # Save the workbook data to the above StringIO object.
@@ -55,13 +50,44 @@ def is_valid_queryparam(param):
 #     # Reposition to the beginning of the StringIO object.
 #     output.seek(0)
 #     # Write the StringIO object's value to HTTP response to send the excel file to the web server client.
-#     response.write(output.getvalue()) 
-
-#     return response                
+#     response.write(output.getvalue())
+#     return response
 
 
 class Index(TemplateView):
     template_name = 'base/index.html'
+
+
+def teste_view(request):
+    forncedores = request.GET["forncedores"]
+    categorias = request.GET["categorias"]
+    produtos = request.GET["produtos"]
+
+    queryset = Produtos.objects.all()
+
+    if is_valid_queryparam(produtos):
+        queryset = queryset.filter(
+            nome=produtos
+        )
+    else:
+        queryset = queryset.all()
+
+    if is_valid_queryparam(categorias):
+        queryset = queryset.filter(
+            categoria__nome=categorias
+        )
+    else:
+        queryset = queryset.all()
+
+    if is_valid_queryparam(forncedores):
+        queryset = queryset.filter(
+            fornecedor__cnpj=forncedores
+        )
+    else:
+        queryset = queryset.all()
+
+    qu_json = serializers.serialize("json", queryset)
+    return HttpResponse(qu_json, content_type="application/json")
 
 class PodutosLista(ListView):
 
@@ -78,48 +104,9 @@ class PodutosLista(ListView):
         return context
 
     def get_queryset(self):
-        queryset = Produtos.objects.all()
+        queryset = Produtos.objects.none()
         return queryset
 
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-
-        forncedores = request.GET.get('forncedores')
-        categorias = request.GET.get('categorias')
-        produtos = request.GET.get('produtos')
-        fornecedor_falta = request.GET.get('fornecedor_falta')
-        categoria_falta = request.GET.get('categoria_falta')
-        produtos_falta = request.GET.get('produtos_falta')
-
-        if is_valid_queryparam(produtos_falta):
-            self.object_list = self.object_list.filter(
-                quantidade=0
-            )
-        else:
-            self.object_list = self.object_list.all()            
-
-        if is_valid_queryparam(produtos):
-            self.object_list = self.object_list.filter(
-                nome=produtos
-            )
-        else:
-            self.object_list = self.object_list.all() 
-
-        if is_valid_queryparam(categorias):
-            self.object_list = self.object_list.filter(
-                categoria__nome=categorias
-            )
-        else:
-            self.object_list = self.object_list.all() 
-
-        if is_valid_queryparam(forncedores):
-            self.object_list = self.object_list.filter(
-                fornecedor__cnpj=forncedores
-            )
-        else:
-            self.object_list = self.object_list.all()   
-
-        return self.render_to_response(self.get_context_data())
 
 
 class ProdutosAdicionar(CreateView):
@@ -135,7 +122,7 @@ class ProdutosAdicionar(CreateView):
     def form_valid(self, form):
         messages.success(
             self.request, self.get_success_message(form.cleaned_data))
-        return redirect(self.get_success_url())            
+        return redirect(self.get_success_url())
 
     def get_context_data(self, **kwargs):
         context = super(ProdutosAdicionar, self).get_context_data(**kwargs)
@@ -162,6 +149,7 @@ class ProdutosAdicionar(CreateView):
 
         return self.form_invalid(form=produtoform)
 
+
 class ProdutoEdit(UpdateView):
 
     template_name = 'produtos/produtos_edit.html'
@@ -174,13 +162,13 @@ class ProdutoEdit(UpdateView):
     def form_valid(self, form):
         messages.success(
             self.request, self.get_success_message(form.cleaned_data))
-        return redirect(self.get_success_url()) 
+        return redirect(self.get_success_url())
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get(self.pk_url_kwarg)
         obj = Produtos.objects.get(id=pk)
         return obj
-        
+
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
 
@@ -188,7 +176,7 @@ class ProdutoEdit(UpdateView):
             instance=self.object, prefix='produtoform')
 
         return self.render_to_response(self.get_context_data(form=produtoform))
-        
+
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
 
@@ -196,23 +184,23 @@ class ProdutoEdit(UpdateView):
             request.POST, request.FILES, instance=self.object, prefix='produtoform', request=request)
 
         if produtoform.is_valid():
-            self.object =  produtoform.save(commit=False)
+            self.object = produtoform.save(commit=False)
             self.object.save()
 
             return self.form_valid(produtoform)
         return self.form_invalid(form=produtoform)
 
+
 class ProdutoDelete(DeleteView):
 
     model = Produtos
     template_name = 'produtos/produtos_confirm_delete.html'
-    success_url = reverse_lazy('core:listprodutos')        
+    success_url = reverse_lazy('core:listprodutos')
     success_message = "Produto deletado com sucesso"
 
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
-        return super(ProdutoDelete, self).delete(request, *args, **kwargs)    
-
+        return super(ProdutoDelete, self).delete(request, *args, **kwargs)
 
 
 # <section>
